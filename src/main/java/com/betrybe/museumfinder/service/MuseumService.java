4f9f6acfe9e805a499b5d5c8cfeebaf2f93fbc05
Matheus Diógenes <mathdiogenes@gmail.com>
2023-11-22 19:14:14 -0300

    feat: R3

---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,39 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Museum service.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+
+  private final MuseumFakeDatabase database;
+
+  public MuseumService(MuseumFakeDatabase database) {
+    this.database = database;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    if (!CoordinateUtil.isCoordinateValid(museum.getCoordinate())) {
+      throw new InvalidCoordinateException();
+    }
+    return this.database.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
