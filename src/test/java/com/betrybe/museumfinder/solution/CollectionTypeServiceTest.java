de8c9cd5f480cdf99cf2f850252ac1c23d21260b
Matheus Diógenes <mathdiogenes@gmail.com>
2023-11-23 14:09:17 -0300

    feat: Search more than one type and returns count as more than 0 test

--- src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
@@ -37,4 +37,20 @@ public class CollectionTypeServiceTest {
     assertTrue(new ReflectionEquals(new CollectionTypeCount(type, 0L))
         .matches(service.countByCollectionTypes(type[0])));
   }
+
+  @Test
+  @DisplayName("Search more than one type and returns count as more than 0 test")
+  void testReturnCountMoreThanZero() {
+    String[] types = {"1", "2"};
+    Mockito
+        .when(database.countByCollectionType("1"))
+        .thenReturn(1L);
+    Mockito
+        .when(database.countByCollectionType("2"))
+        .thenReturn(1L);
+
+    CollectionTypeService service = new CollectionTypeService(database);
+    assertTrue(new ReflectionEquals(new CollectionTypeCount(types, 2L))
+        .matches(service.countByCollectionTypes("1,2")));
+  }
 }
